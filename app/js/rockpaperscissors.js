ec8f1c7b184a60bb7538402eba2318ffb2808138
Dongyang <Dongyang@DongyangmatoMacBook-Air.local>
2016-04-21 16:19:14 +1000

    last version

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -6,7 +6,10 @@
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
     return prompt();
-}
+};
+
+// randomPlay working correctly.
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -15,41 +18,83 @@ function randomPlay() {
         return "paper";
     } else {
         return "scissors";
-    }
-}
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
+    };
+};
 
-function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
-}
+ // playerMove working correctly.
 
-function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+function playerMove(move) {
+  return move || getInput();
+};
+
+
+
+var computerMove = function (move) {
+  return move || randomPlay();
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
-}
+function getWinner(playerMove,computerMove)
+// somehow could not put function playerMove() computerMove() into this parameter
+{
 
-function playToFive() {
+  if ( playerMove == "rock" && computerMove == "paper") {
+       winner = "computer";
+  }
+
+  // Double equals on winner. winner == "player"
+
+  else if (playerMove == "rock" && computerMove == "scissors") {
+       winner = "player";
+  }
+    else if ( playerMove == "scissors" && computerMove == "paper") {
+       winner = "player";
+  }
+  else if ( playerMove == "scissors" && computerMove == "rock") {
+      winner = "computer";
+  }
+    else if ( playerMove == "paper" && computerMove == "rock") {
+       winner = "player";
+  }
+    else if ( playerMove == "paper" && computerMove == "scissors") {
+       winner = "computer";
+  }
+    else{
+       winner = "It's a tie";
+  }
+  return winner
+  }
+
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+
+
+function playToFive() {
+
+
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
-    return [playerWins, computerWins];
-}
 
+
+    while ( (playerWins < 5 ) && (computerWins < 5) ) {
+
+      // I moved these variablies inside this loop because they were part of your problem. Every time pMov gets called, it runs a function and redefines itself based on the input.
+      // That's fine in this loop, because you WANT it to run the whole thing again every time the loop runs, but not anywhere else.
+
+      var pMov = playerMove();
+      var cMov = computerMove();
+      var winner = getWinner(pMov, cMov);
+
+
+      if ( winner === "player" ) {
+        playerWins++;
+      }  else if ( winner === "computer") {
+        computerWins++;
+      };
+
+
+  console.log('Player:' + pMov + ',computer: ' + cMov + '   WINNER:' + winner + '   CurrenlyScore:' + playerWins + ':' + computerWins )
+  };
+  return [playerWins, computerWins];
+};
+playToFive()
